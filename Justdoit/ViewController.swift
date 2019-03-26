a6aee2632c63e0f07e225f96f7e2134ba7baff5a
Subong Jeon <subongjeon@cpp.edu>
2019-03-25 20:45:01 -0700

    Initial Commit

---
+++ Justdoit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Justdoit
+//
+//  Created by James on 3/25/19.
+//  Copyright © 2019 J&L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
