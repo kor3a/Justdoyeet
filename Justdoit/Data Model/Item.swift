d1ea64bfe4f52c6848d46a92d22cbf79654e9a7a
Subong Jeon <subongjeon@cpp.edu>
2019-03-31 15:54:39 -0700

    Convert to MVC design pattern

---
+++ Justdoit/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Justdoit
+//
+//  Created by James on 3/31/19.
+//  Copyright © 2019 J&L. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
